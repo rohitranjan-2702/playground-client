4156172dba382719f17a05206a6a70c4aa146925
ROHIT RANJAN SINGH <rohitranjan2702@gmail.com>
2024-04-30 16:24:40 +0530

    server lad resolved

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -25,6 +25,7 @@ export interface File {
 
 export default function Example() {
   const [loaded, setLoaded] = useState(false);
+  const [serverLoaded, setServerLoaded] = useState(false);
   const socket = useSocket("pgId");
   const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
   const [fileStructure, setFileStructure] = useState<File[]>([]);
@@ -40,6 +41,17 @@ export default function Example() {
     }
   }, [socket]);
 
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://pgserver.onrender.com/");
+      if (response.ok) {
+        setServerLoaded(true);
+        console.log("response ok");
+      }
+    }
+    fetchData();
+  }, []);
+
   const onSelect = (file: File) => {
     socket?.emit("fetchContent", { path: file.path }, (data: string) => {
       file.content = data;
@@ -52,49 +64,57 @@ export default function Example() {
 
   return (
     <div className="flex w-full h-screen">
-      <PanelGroup direction="horizontal" className="">
-        <Panel className="bg-slate-700 " minSize={10} defaultSize={15}>
-          <Sidebar>
-            <div className="h-full w-full bg-slate-700">
-              <h1 className="text-white text-lg w-full bg-zinc-900 p-2">
-                Files
-              </h1>
-              <div>
-                {fileStructure.map((file) => (
-                  <div
-                    key={file.path}
-                    onClick={() => onSelect(file as File)}
-                    className="text-white p-2 border-1 border-black bg-gray-800 hover:bg-gray-700 cursor-pointer"
-                  >
-                    {file.name}
-                  </div>
-                ))}
+      {serverLoaded && (
+        <PanelGroup direction="horizontal" className="">
+          <Panel className="bg-slate-700 " minSize={10} defaultSize={15}>
+            <Sidebar>
+              <div className="h-full w-full bg-slate-700">
+                <h1 className="text-white text-lg w-full bg-zinc-900 p-2">
+                  Files
+                </h1>
+                <div>
+                  {fileStructure.map((file) => (
+                    <div
+                      key={file.path}
+                      onClick={() => onSelect(file as File)}
+                      className="text-white p-2 border-1 border-black bg-gray-800 hover:bg-gray-700 cursor-pointer"
+                    >
+                      {file.name}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          </Sidebar>
-        </Panel>
-        <PanelResizeHandle className="w-1 bg-slate-400/60" />
-        <Panel minSize={50} defaultSize={50}>
-          {socket && <CodeEditor socket={socket} selectedFile={selectedFile} />}
-        </Panel>
-        <PanelResizeHandle className="w-1 bg-slate-500" />
-        <Panel minSize={20}>
-          <PanelGroup direction="vertical" className="bg-slate-300">
-            <Panel>
-              {/* OUTPUT */}
-              <div className="w-full h-full">
-                <iframe width={"100%"} height={"100%"} src={`${OUTPUT_URL}`} />
-              </div>
-            </Panel>
-            <PanelResizeHandle className="h-1 bg-slate-500" />
-            <Panel className="bg-black text-white">
-              <div className="w-full ">
-                {socket && <TerminalComponent socket={socket} />}
-              </div>
-            </Panel>
-          </PanelGroup>
-        </Panel>
-      </PanelGroup>
+            </Sidebar>
+          </Panel>
+          <PanelResizeHandle className="w-1 bg-slate-400/60" />
+          <Panel minSize={50} defaultSize={50}>
+            {socket && (
+              <CodeEditor socket={socket} selectedFile={selectedFile} />
+            )}
+          </Panel>
+          <PanelResizeHandle className="w-1 bg-slate-500" />
+          <Panel minSize={20}>
+            <PanelGroup direction="vertical" className="bg-slate-300">
+              <Panel>
+                {/* OUTPUT */}
+                <div className="w-full h-full">
+                  <iframe
+                    width={"100%"}
+                    height={"100%"}
+                    src={`${OUTPUT_URL}`}
+                  />
+                </div>
+              </Panel>
+              <PanelResizeHandle className="h-1 bg-slate-500" />
+              <Panel className="bg-black text-white">
+                <div className="w-full ">
+                  {socket && <TerminalComponent socket={socket} />}
+                </div>
+              </Panel>
+            </PanelGroup>
+          </Panel>
+        </PanelGroup>
+      )}
     </div>
   );
 }
