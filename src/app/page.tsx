8908e707c37e85cfbf080476deca04da03f78d5a
ROHIT RANJAN SINGH <rohitranjan2702@gmail.com>
2024-04-30 21:16:14 +0530

    working...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSocket } from "@/hooks/useSocket";
 
 export default function Home() {
   const router = useRouter();
-  const socket = useSocket("pgId");
+  const [serverLoaded, setServerLoaded] = useState(false);
+
+  const handleClick = () => {
+    if (serverLoaded) {
+      router.push("/playground");
+    } else {
+      alert("Server not loaded yet, please wait");
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://pgserver.onrender.com/");
+      if (response.ok) {
+        setServerLoaded(true);
+        console.log("response ok");
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <div className="p-12 w-full h-screen flex bg-black justify-center space-y-6 flex-col items-center">
         <h1 className="text-4xl text-purple-600">Codedamn Playground</h1>
         <button
-          onClick={() => router.push("/playground")}
-          className="block px-4 py-2 rounded-full bg-transparent text-purple-300 ring-1 ring-purple-400">
+          onClick={() => handleClick()}
+          className="block px-4 py-2 rounded-full bg-transparent text-purple-300 ring-1 ring-purple-400"
+        >
           Start Playground
         </button>
       </div>
